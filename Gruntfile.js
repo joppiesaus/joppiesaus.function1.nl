c07d7188b9cedd93fd5b9304a3ac041217e6020d
joppiesaus <job@function1.nl>
2015-09-09 17:00:48 +0200

    Added grunt site minification

---
+++ Gruntfile.js
@@ -0,0 +1,80 @@
+// This is pretty cool!
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    
+    uglify: {
+        build: {
+            files: {
+                "build/js/build.js": ["js/core.js", "js/logo.js", "js/tiles.js"]
+            }
+        }
+    },
+    
+    /*imagemin: {
+        build: {
+            options: {
+                optimizationLevel: 5
+            }
+        },
+        files: [{
+            expand: true,
+            cwd: "img",
+            src: ["img/*.{png,jpg,gif}"],
+            dest: "build/img/"
+        }]
+    },*/
+    
+    cssmin: {
+        build: {
+            files: {
+                "build/css/style.css": ["css/*.css"]
+            }
+        }
+    },
+    
+    processhtml: {
+        build: {
+            files: {
+                "build/index.html": ["new.html"]
+            }
+        }
+    },
+    
+    htmlmin: {
+        build: {
+            options: {
+                removeComments: true,
+                collapseWhitespace: true
+            },
+            files: {
+                "build/index.html": ["build/index.html"]
+            }
+        }
+    },
+    
+    usebanner: {
+        build: {
+            options: {
+                banner: "<!-- Hey! This code is not for humans! View it here instead: http://github.com/joppiesaus/joppiesaus.function1.nl -->"
+            },
+            files: {
+                src: ["build/index.html"]
+            }
+        }
+    }
+    
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  //grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-banner');
+  
+  // Default tasks
+  grunt.registerTask('default', [/*'imagemin',*/ 'uglify', 'cssmin', 'processhtml', 'htmlmin', 'usebanner']);
+
+};
