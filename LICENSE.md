95d9d33c9dc09e2695820be943149f6e7317971a
joppiesaus <job@function1.nl>
2016-03-03 09:17:04 +0100

    Add the skeletons, remove everything else GUYS I TOLD YOU FRONTPAGE EXPRESS IS BETTER THEN DREAMWEAVER

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2015 Job van der Zweep
+Copyright (c) 2015-2016 Job van der Zweep
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
---
+++ js/stuff.js
@@ -0,0 +1,318 @@
+var tileData =
+[
+    {
+        "title": "HOW TO MAKE POPCORN",
+        "content": "Okay listen up I'm only going to say this <b><em>ONCE</em></b> so listen closely I am not going to repeat this again okay this is a famous recipe that's very special so here we go no repeats:First pick some corn that you can pop(now you also know where the name came from), pick a pan and a pan to cover the other pan. Also pick lots of sugar because you like lots of sugar. Don't lie to me.<ol style=\"list-style-type: upper-roman\"><li>Heat up both pans. Make them hot, but make sure you don't burn them. This is not easy I already destroyed lots of pans but my mom never gets mad because I try<li>Now when they're hot put some corn in it. A few table spoons. As much as possible, but make sure they don't stack onto each other.<li>Put the other pan onto the pan<li>Wait. This is the hardest phase. Keep the pan hot, but don't make it too hot, you'll burn the popcorn.<li>Get nervous<li>Question life<li>Hear the relieving sound of popping popcorn<li>Now wait again until the war between the popcorn is finally over<li>Whoa it is war in there<li>Okay turn the gas low, put sugar in it. I reccommend 2 or 3 times the amount of table spoons of sugar as you put corn in it. You can do more of course :))))<li>wait for the sugar to carmilize<li>Get more nervous this time<li>See it carmilizing? Good! Now stir!<li>STIR!<li>JUST A LITTLE MORE...!<li>Done.<li>HAVE FUNS!!!</ol>"
+    },
+    {
+        "title": "License",
+        "content": "<p>All stuff on this page - except otherwise noted - is licensed under <a href=\"http://function1.nl/p/unlicense.txt\">the unlicense</a>. This doesn't mean I don't like credit, but it does mean you can quickly \"steal\" stuff without attribution, since most of it is made to learn anyway.</p><p><a href=\"https://github.com/joppiesaus/joppiesaus.function1.nl\">This website</a> however is licensed under the <a href=\"LICENSE.md\">MIT license</a>.</p><p>I encourage to make your software <a href=\"http://www.gnu.org/philosophy/free-sw.html\">free</a>(as in not having to pay for it and as in freedom, <a href=\"https://static.fsf.org/nosvn/FSF30-video/FSF_30_1080p.webm\">video of that</a>). Because, who <em>doesn't</em> like <a href=\"https://youtu.be/u6XAPnuFjJc\">freedom</a>?<p>"
+    },
+
+    {
+        "title": "3D chair",
+        "description": "Chair in 3D",
+        "image": "img/3Dlearningstuffchair.PNG",
+        "url": "https://www.khanacademy.org/computer-programming/3d-learning/5194335302844416",
+        "tags": ["Web", "JavaScript", "Toy"]
+    },
+    {
+        "title": "ASCII 3D chair",
+        "description": "chair in 3d but then with text",
+        "image": "img/3Dlearningstufftextchair.PNG",
+        "url": "https://www.khanacademy.org/computer-programming/3d-out-of-text/4723472598171648",
+        "tags": ["Web", "JavaScript", "Toy"]
+    },
+
+    {
+        "title": "Motorcycle",
+        "description": "Motorcycle is a C# console (kindof)RPG where you can do what you want. You can race, upgrade your motor, and that's it I believe. It was my first console game, and it is really boring actually. It also features bad code. Bad. Bad code. But hey! I've learned from it!",
+        "image": "img/motorcycle.PNG",
+        "links": [["Download", "dl/SuperSOURCE.zip"]],
+        "tags": ["Windows", "Console", "C#", "Game"]
+    },
+    {
+        "title": "SuperHero",
+        "description": "SuperHero is my second C# console RPG which is a lot better than Motorcycle. With random and weird things, easter eggs, and some other geeky stuff! It was never finished actually. If you are interested, you can develop it by yourself and if you <a href=\"mailto:job@function1.nl\">send me an email</a> I will post it on my website! I'll be very happy if you do that.<br>Have fun enyoying your <u><b>SUPERPOWERRRSSS</b></u>!!!1",
+        "image": "img/powerz.PNG",
+        "links": [["Download", "dl/cycle_SRC.zip"]],
+        "tags": ["Windows", "Console", "C#", "Game"]
+    },
+
+    {
+        "title": "Windows XP's 3D \"Space Cadet\" Pinball",
+        "description": "YES! Another wormhole! One of my favorite games when I was 5 or something. Hours of fun! Runs on Windows 7, 8 and even Linux(via Wine)! Have fun!",
+        "image": "img/Pinball.PNG",
+        "links": [["English", "dl/Pinball_en.zip"], ["Dutch", "dl/Pinball_nl.zip"]],
+        "tags": ["Windows", "Game"]
+    },
+    {
+        "title": "I AM A SPACECRAFT",
+        "description": "So I browsed my projects directory, and I suddenly saw this! It's my very first (XNA) game! I remember it getting declined on indieDB... I also figured out the comets are stolen from <a href=\"http://defcom1.net/xnalessons/xnalevel1lesson05.php\">here</a>, wait... this whole game is from here! Well not completely(*relieving sigh*), because mine has enemys, and powerups, screenshots, and much, much more!<br>Unfortunatly I lost the source code for that. :( It's actually a playable game(!!!), so I decided to give it a honourfull place like here. My highscore is somewhere arround 3000. Have fun with it!",
+        "links": [["Binary", "dl/I_AM_A_SPACECRAFT.zip"]],
+        "image": "img/immaspacecraaft.PNG",
+        "tags": ["Windows", "C#", "XNA", "Game"]
+    },
+    {
+        "title": "chiken simulater",
+        "image": "img/csgochiken.png",
+        "description": "You can now be a chiken! Search your favorite streamer, enter his ID and turn auto chiken on! Made in an hour or so.",
+        "links": [["Binary", "dl/chiken_simulater.zip"], ["Source", "dl/chiken_simulater_src.zip"]],
+        "tags": ["Windows", "C#", "Tool"]
+    },
+    {
+        "title": "Click Game",
+        "image": "img/click.PNG", // Bad name. Bad name.
+        "description": "A stupid game I made in C#. It's stupid. It yells at you. It's frustrating, and intense. But it has a degree of fun and polish. Your objective is to click the buttons in order in a certain time, which get's harder over the rounds you've played.",
+        "links": [["Binary", "dl/Click_Game.zip"], ["Source", "dl/Click_Game_SRC.zip"]],
+        "tags": ["Windows", "C#"]
+    },
+    {
+        "title": "Low-Memory Candy",
+        "image": "img/fry.PNG",
+        "description": "You are a man, whose memory is weird. It needs candies! Otherwise it will forget stuff! Can you keep his memory intact?<br>Made for the <a href=\"http://itch.io/jam/candyjam\">Candyjam</a>!",
+        "links": [["Binary", "dl/LMC_bin.zip"], ["Source", "dl/LMC_src.zip"], ["Submission page", "http://joppiesaus.itch.io/low-memory-candy"]],
+        "tags": ["Windows", "C#", "XNA", "Game"]
+    },
+    {
+        "title": "\"Zombie Game\"",
+        "image": "img/zombiegeamzrage.PNG",
+        "description": "A simple little zombie game I made using <a href=\"http://youtu.be/81AcHa15H9c\">these</a> tutorials to learn more about C++ game development! It was really fun to make! And the game <em>is</em> really fun too!",
+        "links": [["Page", "dl/zombiegame"]],
+        "tags": ["Windows", "Linux", "C++", "Game"]
+    },
+    {
+        "title": "FAG.com",
+        "image": "img/fagfagfag.PNG",
+        "description": "This DOS program written in 8086 Assembly calls you a <em>fag</em>(a loser, a bundle sticks of wood) 3 times with an \"!\" after it. Since it's written in 8086 Assembly, it's only for OS-es which support 16 bit.<br>It may be violent, stupid, and ridiculous, but hey, it's only 32 bytes.",
+        "links": [["FAG.com", "dl/FAG.com"], ["FAG.asm", "dl/FAG.asm"], ["FAG.asm zipped", "dl/FAG_src.zip"]]
+    },
+    {
+        "title": "universe.zip",
+        "description": "This is what happens when you give me time and confidence and a secret ingredient(which I don't know). You get things like a zip file of 295 KB, which contains 16^19 GiB(67108864 Yobibyte) of useless compressed data(all 0's).<br>It's so useless, you could even bully it because only what it does is using space. That's all.",
+        "links": [["universe.zip", "dl/universe.zip"], ["1 GiB file gen cpp", "dl/Useless1GiBDataGenerator.cpp"]]
+    },
+    {
+        "title": "Windows 7 Spider Solitaire trainer-hacker 2015 XTREME MLG EDITION",
+        "image": "img/mlghckr.PNG",
+        "description": "The trainer you use when you're bored. I've learned A LOT from it. I spend days working with code and even <a href=\"http://stackoverflow.com/q/26782399/2622807\">asking and answering my own stackoverflow question</a>... It's the neatest thing I've ever programmed.",
+        "links": [["x64", "dl/CPPW7SSTH2015XTREMEMLGEDITIONx64.zip"], ["x86", "dl/CPPW7SSTH2015XTREMEMLGEDITIONx86.zip"], ["Source", "dl/CPPW7SSTH2015XTREMEMLGEDITION.cpp"]],
+        "tags": ["Windows", "C++", "Tool"]
+    },
+    {
+        "title": "RedBull Santa",
+        "image": "img/santa.PNG",
+        "description": "RedBull Santa! A C# XNA game, which is very trippy and sound-dynamic! You must prevent santa from falling-of the \"roof\", while drinking as many as possible Red Bull!<br><span class=\"warning\">Contains flashing images.</span>",
+        "links": [["Binary", "dl/RedBull_Santa.zip"], ["Source", "dl/RBS_src.zip"]],
+        "tags": ["Windows", "C#", "XNA", "Game"]
+    },
+    {
+        "title": "Offensive Defense",
+        "image": "img/od.PNG",
+        "description": "Offensive defense is a strategy defense game made for the Ludum Dare October Challenge 2014, where the target is to finish a game in October and earn 1 $!<br><br>It horribly failed.<br><br>But it's a fun game! Or fun... It will provoke human reactions!",
+        "links": [["Page", "http://function1.nl/p/od/"], ["Binary(WIN XNA)", "http://function1.nl/p/od/dl/Offensive%20Defense%20WIN%20XNA.zip"], ["Source", "http://function1.nl/p/od/dl/OffenseDefense_src.zip"], ["Mediocre trailer", "https://youtu.be/7Jhp71hCBK4"]],
+        "tags": ["Windows", "C#", "XNA", "Game", "Ludum Dare"]
+    },
+    {
+        "title": "SuperDrive OVERCLOCK",
+        "image": "img/ld/m49.PNG",
+        "description": "You are a DVD/CD-ROM Driver laser. Your task is to read all the data from the disk as long as possible. You can miss 3 times. If you miss more than that, the computer will crash. But the driver is hacked & overclocked by the user.<br>How long can you read data from the disc?",
+        "links": [["Submission Page", "http://www.ludumdare.com/compo/minild-49/?action=preview&uid=29014"], ["Binary", "http://function1.nl/ld/mini/LD49/Mini-LD49.zip"], ["Source", "http://function1.nl/ld/mini/LD49/Mini-LD49_src.zip"]],
+        "tags": ["Windows", "C#", "XNA", "Game", "Ludum Dare"]
+    },
+    {
+        "title": "Bob the different bird",
+        "image": "img/ld/32.PNG",
+        "description": "Bob was always a different bird. But today, he decides to do something completely different.",
+        "links": [["Submission Page", "http://www.ludumdare.com/compo/ludum-dare-32/?action=preview&uid=29014"], ["Binary", "http://function1.nl/ld/mini/LD32/LD32_Bob.zip"], ["Source", "http://function1.nl/ld/mini/LD32/LD32_Bob_src.zip"], ["Timelapse", "https://youtu.be/KFcB7GkhuNE"]],
+        "tags": ["Windows", "C#", "XNA", "Game", "Ludum Dare"]
+    },
+    {
+        "title": "\"Stars.EXE\"",
+        "image": "img/stars.PNG",
+        "description": "It was a nice \"Test project\", where I did all sorts of (useless) stuff. It was originally created to create my old website's background, but I managed to make a *nice* game from it. Comes with a *nice* *config format*, a bad name and reasonable code.",
+        "links": [["Binary", "dl/Stars.zip"], ["Source", "dl/Stars_4_src.zip"]],
+        "tags": ["Windows", "C#", "XNA", "Game"]
+    },
+    {
+        "title": "Console Four in a Row",
+        "image": "img/row.PNG",
+        "description": "Something revolutionary... You can play four in a row on your PC! It includes the source code & executable. You can define how many columns and rows you want. You can even pick 1000!",
+        "links": [["Download", "dl/4InARow_Src.zip"]],
+        "tags": ["Windows", "Console", "C#", "Game"]
+    },
+    {
+        "title": "SuperForeverAloneInThaDungeon",
+        "image": "img/sfaitdinv.PNG",
+        "description": "SFAITD is a Rogue-Like made to learn more about object-oriented programming. It was another C# Console game, but this time with text as the level. I didn't now such a thing existed until I searched arround the internet. I worked on it for a long time and never finished it, but It's something!",
+        "links": [["Page", "http://function1.nl/p/sfaitd/"]],
+        "tags": ["Windows", "Console", "C#", "Game"]
+    },
+    {
+        "title": "untitled.exe",
+        "image": "img/untitleddotexe.PNG",
+        "description": "untitled.exe. A very early programming project, which prints variants of food on the screen. Its code is bad, but that's why I learnt from it. The directory structure is also interesting. Too interesting. I give you everything. I do not even delete the <code>obj</code> folder. The exe files do not match with the code, either. The dates are weird. But mostly, the program is weird. But cool, at least.",
+        "links": [["Solve that mystery!(or just have fun)", "dl/untitleddotexe.zip"]],
+        "tags": ["Windows", "Console", "C#", "Toy"]
+    },
+
+
+    {
+        "title": "C++ Apple Loser",
+        "image": "img/bravepcuser.PNG",
+        "description": "<span class=\"warning\">This program creates milions of apples but forgets where they are located. Notice your RAM.</span><br>I LOVE pointers! This is made to learn more about pointers. What this program does:<ol><li>Create a new apple and assign it to a pointer<li>Create a new apple and assign it to the same pointer<li>Goto 1</ol>This program will fire up your RAM and will throw a <code>bad_alloc</code>",
+        "links": [["Binary(Windows)", "dl/apple_loser.zip"], ["Source", "dl/apple_loser_cpp.zip"]],
+        "tags": ["Windows", "Linux", "C++"]
+    },
+    {
+        "title": "never_thrust_batch_files.bat",
+        "description": "So I thought: How can I let someone rage while not harming the whole computer? It's easy! So I came up with this: Your disk drive will open, you are unable to type correctly, you get spammed and the computer yells to you, and much more... I present to you: never_thrust_batch_files.bat!<br>Please use this for \"fun\" purposes, not to harm people. It's easy to fix, just open run and type in <code>msconfig</code>, then open the tab \"startup\" and <code>remove never_thrust_batch_files.bat</code> from the list. I still haven't tested it, thought.",
+        "image": "img/ntbf.PNG",
+        "links": [["Download", "dl/never_thrust_batch_files.bat"]],
+        "tags": ["Windows", "Batch"]
+    },
+    {
+        "title": "installer.bat",
+        "description": "Bing bar? Conduit toolbar!? Norton Antivirus!?! That trash wouldn't be able to beat THIS! Designed just for you, to generate trash. If you have an old computer, you want to bomb, just enter -1 trash! <em>Die in style</em>.",
+        "image": "img/itrash.PNG",
+        "links": [["Download!", "dl/installer.bat"]],
+        "tags": ["Windows", "Batch"]
+    },
+
+    {
+        "title": "NEON IT ALLL",
+        "onclick": "var style=document.createElement('style');style.innerHTML='*{text-shadow:0 0 3px #f00, 0 0 5px #00f !important}h1,h2{text-shadow:0 0 8px #f00, 0 0 15px #00f !important}';document.getElementsByTagName('head')[0].appendChild(style)",
+    },
+
+    {
+        "title": "I like this site",
+        "onclick": "var a=['github.com','glslsandbox.com','youtu.be','khanacademy.org','ludumdare.com','cachemonet.com'];window.open('http://'+a[Math.floor(Math.random()*a.length)])",
+        "tags": ["Web"]
+    },
+    {
+        "title": "Blog",
+        "url": "http://joppiesaus.github.io",
+    },
+    {
+        "title": "Archive",
+        "url": "/archive"
+    },
+    {
+        "title": "Cats!",
+        "url": "dl/examples/igm"
+    },
+
+    {
+        "title": "Donations",
+        "content": "If you <em>really</em> like my work, you can donate me your crappy CSGO skins(I love crappy CSGO skins), give me TF2 items, give me your stupid games, or other trash or not-so-trash on <a href=\"https://steamcommunity.com/tradeoffer/new/?partner=150881846&token=LdOYlDqJ\">steam</a>!"
+    }
+];
+
+var linkpictures = [
+    "candlecake.gif",
+    "cellflash.gif",
+    "floppysave.gif",
+    "hot.gif",
+    "point.gif",
+    "lavalamp_blue.gif",
+]
+
+var grid;
+console.log("hi");
+
+// Gets a DOM element by id
+function ge(id)
+{
+    return document.getElementById(id);
+}
+
+var gridCreateLink = function(element, title, location)
+{
+    var a = document.createElement("a");
+
+    var img = document.createElement("img");
+    img.src = "img/oldweb/" + linkpictures[Math.floor(Math.random() * linkpictures.length)];
+    img.alt = title;
+    a.appendChild(img);
+
+    a.href = location;
+    a.innerHTML += title;
+    element.appendChild(a);
+}
+
+window.onload = function()
+{
+    grid = ge("grid");
+
+    tileData.forEach(function(e){
+        var row = document.createElement("tr");
+        var firstColumn = document.createElement("td");
+
+        if (e.image)
+        {
+            var image = document.createElement("img");
+            image.src = e.image;
+            image.alt = e.image;
+            firstColumn.appendChild(image);
+        }
+
+        if (e.title)
+        {
+            var title = document.createElement("span");
+            title.className = "title";
+            title.textContent = e.title;
+            firstColumn.appendChild(title);
+        }
+
+        if (e.content)
+        {
+            firstColumn.innerHTML += e.content;
+        }
+        else if (e.description)
+        {
+            firstColumn.innerHTML += e.description;
+        }
+        else if (e.onclick)
+        {
+            firstColumn.className += "clickable";
+            firstColumn.innerHTML += "Click here ;-)";
+            firstColumn.onclick = eval("(function(){" + e.onclick + "})");
+        }
+        else
+        {
+            firstColumn.innerHTML += "NO DESCRIPTION :((( I CRY";
+        }
+
+        var secondColumn = document.createElement("td");
+        var fail = false;
+
+        if (e.url)
+        {
+            gridCreateLink(secondColumn, "Page", e.url);
+        }
+        else if (e.links)
+        {
+            e.links.forEach(function(link){
+                gridCreateLink(secondColumn, link[0], link[1]);
+            });
+        }
+        else
+        {
+            firstColumn.colSpan = "2";
+            fail = true;
+        }
+
+        row.appendChild(firstColumn);
+
+        if (!fail)
+        {
+            row.appendChild(secondColumn);
+        }
+        else
+        {
+            secondColumn = undefined;
+        }
+
+        grid.appendChild(row);
+    });
+};
